1a8e66cd5e15454828ac828d89eb91c14494db88
zhangqi <zhangqihappyzq@163.com>
2020-05-08 16:58:16 +0800

    加入详情页

--- src/navigation2/App.js
+++ src/navigation2/App.js
@@ -19,13 +19,27 @@ function HomeScreen() {
     );
 }
 
+function DetailsScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Details Screen</Text>
+
+        </View>
+    );
+}
+
 const Stack = createStackNavigator();
 
 function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} />
+
+                <Stack.Screen name="Home" component={HomeScreen }
+                              options={{ title: 'Overview' }}/>
+                <Stack.Screen name="Details" component={DetailsScreen} />
+                {props => <HomeScreen {...props} extraData={someData} />}
+
             </Stack.Navigator>
         </NavigationContainer>
     );
