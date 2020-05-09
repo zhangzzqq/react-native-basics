da27b6ee75078d0e8a8c7154fd38a29d158c7c59
zhangqi <zhangqihappyzq@163.com>
2020-05-09 09:57:55 +0800

    增加底部图标

--- src/navigation2/TabNavigation.js
+++ src/navigation2/TabNavigation.js
@@ -1,26 +1,66 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import {Button, Text, View} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-
-function HomeScreen() {
+import Icon from 'react-native-vector-icons/Ionicons';
+function HomeScreen({navigation}) {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Home!</Text>
+            <Button
+                title="Go to Settings"
+                onPress={() => navigation.navigate('Settings')}
+            />
         </View>
     );
 }
 
-function SettingsScreen() {
+function SettingsScreen({navigation}) {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Settings!</Text>
+            <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
         </View>
     );
 }
 
 const Tab = createBottomTabNavigator();
+
+
+function IconWithBadge({ name, badgeCount, color, size }) {
+    return (
+        <View style={{ width: 24, height: 24, margin: 5 }}>
+            <Icon name={name} size={size} color={color} />
+            {badgeCount > 0 && (
+                <View
+                    style={{
+                        // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
+                        position: 'absolute',
+                        right: -6,
+                        top: -3,
+                        backgroundColor: 'red',
+                        borderRadius: 6,
+                        width: 12,
+                        height: 12,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+                        {badgeCount}
+                    </Text>
+                </View>
+            )}
+        </View>
+    );
+}
+
+
+function HomeIconWithBadge(props) {
+    // You should pass down the badgeCount in some other ways like React Context API, Redux, MobX or event emitters.
+    return <IconWithBadge {...props} badgeCount={3} />;
+}
+
 
 export default function TabNavigaton() {
     return (
@@ -29,7 +69,6 @@ export default function TabNavigaton() {
                 screenOptions={({ route }) => ({
                     tabBarIcon: ({ focused, color, size }) => {
                         let iconName;
-
                         if (route.name === 'Home') {
                             iconName = focused
                                 ? 'ios-information-circle'
@@ -39,7 +78,7 @@ export default function TabNavigaton() {
                         }
 
                         // You can return any component that you like here!
-                        return <Ionicons name={iconName} size={size} color={color} />;
+                        return <Icon name={iconName} size={size} color={color} />;
                     },
                 })}
                 tabBarOptions={{
