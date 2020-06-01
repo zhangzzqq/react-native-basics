d95f93068e86b3e4d2fbd963411bc989c17d1472
zhangqi <zhangqihappyzq@163.com>
2020-06-01 23:24:51 +0800

    popup-menu

--- es6Study/test/Test.js
+++ es6Study/test/Test.js
@@ -1,38 +1,11 @@
+let promise = new Promise((resolve, reject)=>{
+    reject("拒绝了");
+    resolve("又通过了");
+});
+promise.then((data)=>{
+    console.log('success' + data);
+}, (error)=>{
+    console.log(error)
+});
 
-// let a = undefined? '12':'34';
-// let a = null? '12':'34';
-
-// console.log(a);
-
-// const runes = require('runes');
-// const usaText = "A🇺🇸Z";
-// // runes.substr(usaText, 0, 2); // "A🇺🇸"
-// console.log(runes.substr(usaText, 0, 2));
-
-
-// let a ='qq';
-// console.log(a>0);
-
-function ArrayIsEqual(arr1,arr2){//判断2个数组是否相等
-    if(arr1===arr2){//如果2个数组对应的指针相同，那么肯定相等，同时也对比一下类型
-        return true;
-    }else{
-        if(arr1.length!=arr2.length){
-            return false;
-        }else{//长度相同
-            for(let i in arr1){//循环遍历对比每个位置的元素
-                if(arr1[i]!=arr2[i]){//只要出现一次不相等，那么2个数组就不相等
-                    return false;
-                }
-            }//for循环完成，没有出现不相等的情况，那么2个数组相等
-            return true;
-        }
-    }
-}
-
-var s = ArrayIsEqual([1, '2', 3], [1, 2, 3]);
-var t = ArrayIsEqual([1, 2, 4,3], [1, 2, 3,8,9]);
-
-console.log(s);
-console.log(t);
 
---
+++ src/popup-menu/Demo.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import Menu from 'react-native-popup-menu';
+
+import Example from './Example';
+import BasicExample from './BasicExample';
+import ControlledExample from './ControlledExample';
+import ExtensionExample from './ExtensionExample';
+import ModalExample from './ModalExample';
+import StylingExample from './StylingExample';
+import NonRootExample from './NonRootExample';
+import TouchableExample from './TouchableExample';
+import MenuMethodsExample from './MenuMethodsExample';
+import CloseOnBackExample from './CloseOnBackExample';
+import FlatListExample from './FlatListExample';
+import InFlatListExample from './InFlatListExample';
+import PopoverExample from './PopoverExample';
+
+const demos = [
+  { Component: BasicExample, name: 'Basic example' },
+  { Component: Example, name: 'Advanced example' },
+  { Component: ControlledExample, name: 'Controlled example' },
+  { Component: MenuMethodsExample, name: 'Controlling menu using menu methods' },
+  { Component: ExtensionExample, name: 'Extensions example' },
+  { Component: ModalExample, name: 'Modal example' },
+  { Component: StylingExample, name: 'Styling example' },
+  { Component: TouchableExample, name: 'Touchable config example' },
+  { Component: NonRootExample, name: 'Non root example' },
+  { Component: CloseOnBackExample, name: 'Close on back button press example' },
+  { Component: FlatListExample, name: 'Using FlatList' },
+  { Component: InFlatListExample, name: 'Menu in FlatList' },
+  { Component: PopoverExample, name: 'Popover renderer' },
+];
+
+// show debug messages for demos.
+Menu.debug = true;
+
+export default class Demo extends Component {
+  constructor(props, ctx) {
+    super(props, ctx);
+    this.state = {
+      selected: undefined,
+    };
+  }
+  render() {
+    if (this.state.selected) {
+      return <this.state.selected/>;
+    }
+    return (
+        <View style={styles.container}>
+          <View>
+            <Text>Select example:</Text>
+            {demos.map(this.renderDemo, this)}
+          </View>
+        </View>
+    );
+  }
+
+  renderDemo(demo, idx) {
+    const type = idx + '. ' + demo.name;
+    return (
+      <TouchableHighlight key={type} onPress={()=> this.setState({selected: demo.Component})}>
+        <View>
+          <Text>{type}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'column',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+});
