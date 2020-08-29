5aa5b3c50783d45cbc8fdeb6e1ca307bedaa691c
zhangqi <zhangqihappyzq@163.com>
2020-08-29 17:04:40 +0800

    之前test代码

--- src/test/test.js
+++ src/test/test.js
@@ -1,12 +1,59 @@
-function sum(...theArgs) {
-    return theArgs.reduce((a, c) => {
-      return a + c;
-    });
-  }
+
+
+let a = [
+    [
+      {
+        name: '我的收藏',
+      },
+      {
+        name: '套餐信息',
+      },
+      {
+        name: '事件分析',
+      },
+      {
+        name: '专题配置',
+      }],
+    [
+      {
+        name: '系统设置',
+      }, {
+        name: '版本历史',
+      }, {
+        name: '联系我们',
+      }],
   
-  console.log(sum(1, 2, 3));
-  // expected output: 6
-  
-  console.log(sum(1, 2, 3, 4));
-  // expected output: 10
-  
+    [
+      {
+        name: '分享中移舆情',
+      }, {
+        name: '检查更新',
+      }]
+  ];
+
+  let b =  [
+    {
+      name: '我的收藏',
+    },
+    {
+      name: '套餐信息',
+    },
+    {
+      name: '事件分析',
+    },
+    {
+      name: '专题配置',
+    }];
+
+  //   b.splice(1,1);
+  // console.log(b)
+  a.splice(1,1);
+  console.log(a)
+// // let a = [{a1:'1',a2:'2'},{a3:'3',a4:'4'},]
+// for (let item of a) {
+//     // console.log(a)
+//     console.log(item[0, 1])
+//     // delete item.name=='我的收藏'
+// }
+// a.splice(0,0);
+// console.log(a)
---
+++ src/test/test2.js
@@ -0,0 +1,3 @@
+let a  = Number("2.2.3");
+
+console.log(a)
